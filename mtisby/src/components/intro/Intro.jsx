a611d6768d3675914031560c9a1a9f10d488d7e2
mtisby <marieltisby1998@gmail.com>
2021-11-25 18:11:03 -0800

    styling

--- mtisby/src/components/intro/Intro.jsx
+++ mtisby/src/components/intro/Intro.jsx
@@ -4,8 +4,7 @@ const Intro = () => {
     return (
         <div className="intro">
             <div className="nameAndDescript">
-                {/* <h1 class="name">Mariel Tisby </h1>
-                <p class="description">fullstack developer</p> */}
+                <img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1637889344/portfolio/2021-11-24_15-41-21_Trim_SparkVideo_qudgz1.gif" alt="" />
             </div>
         </div>
     )
--- mtisby/src/components/portfolio/Portfolio.jsx
+++ mtisby/src/components/portfolio/Portfolio.jsx
@@ -30,51 +30,38 @@ const Portfolio = () => {
 
     return (
         <div className='portfolio'>
-            <h1 className="portfolioHeaderh1">My Stack</h1>
-            <div className="myStack">
-                <img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636756690/portfolio/logos/github-logo_lzcuqe.png" alt="github-logo" />
-                <img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636756692/portfolio/logos/javascript-logo_ccplff.svg" alt="js-logo" />
-                <img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636756694/portfolio/logos/nodejs-logo_ohziem.png" alt="nodejs-logo" />
-                <img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636756696/portfolio/logos/express-routing-logo_iwpb6y.png" className="express" alt="express-logo" />
-                <img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636756698/portfolio/logos/python-logo_fzoyn9.png" alt="python-logo" />
-                <img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636756700/portfolio/logos/linux-logo_cn6qiw.png" alt="linux-logo" />
-                <img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636756702/portfolio/logos/react-logo_qsqhgf.png" alt="react-logo" />
-                <img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636756704/portfolio/logos/visual-studio-code-logo_yoai3t.png" alt="vs-code-logo" />
-                <img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636756706/portfolio/logos/terminal-logo_jwxwmc.png" alt="terminal-logo" />
-                <img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636757918/portfolio/logos/mongodb_ng1uxj.png" className="express" alt="mongodb-logo" />
-            </div>
             <h1 className="portfolioHeaderh1">Projects</h1>
             <div className="projects">
             <div className="projectDiv">
                     <h1 className="projectHeader">Poodle Doodle</h1>
                 <img onMouseEnter={handleHoverEnter} onMouseLeave={ handleHoverLeave } src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1637557563/paint-app/poodle-doodle_gkby0l.png" alt="poodle doodle website gif" id="poodle-doodle"/>
                     <div className="buttonDiv">
-                        <button className="projectButtons"><a href="https://pacific-bastion-50258.herokuapp.com/">website</a></button>
-                        <button className="projectButtons"><a href="https://github.com/mtisby/paint-app">repository</a></button>
+                        <button className="projectButtons"><a href="https://pacific-bastion-50258.herokuapp.com/"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1637450368/paint-app/logos/Website_pt9szx.png" alt="website" /></a></button>
+                        <button className="projectButtons"><a href="https://github.com/mtisby/paint-app"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1637450368/paint-app/logos/GitHub_qb49mi.png" alt="github-logo" /></a></button>
                     </div>
                 </div>
                 <div className="projectDiv">
                     <h1 className="projectHeader">Cheflavor</h1>
                     <img onMouseEnter={handleHoverEnter} onMouseLeave={ handleHoverLeave } src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636656191/portfolio/cheflavor_dzfi1a.png" alt="cheflavor website gif" id="cheflavor"/>
                     <div className="buttonDiv">
-                        <button className="projectButtons"><a href="https://protected-scrubland-89262.herokuapp.com/cheflavor">website</a></button>
-                        <button className="projectButtons"><a href="https://github.com/mtisby/cheflavor">repository</a></button>
+                        <button className="projectButtons"><a href="https://protected-scrubland-89262.herokuapp.com/cheflavor"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1637450368/paint-app/logos/Website_pt9szx.png" alt="website" /></a></button>
+                        <button className="projectButtons"><a href="https://github.com/mtisby/cheflavor"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1637450368/paint-app/logos/GitHub_qb49mi.png" alt="github-logo" /></a></button>
                     </div>
                 </div>
                 <div className="projectDiv">
                     <h1 className="projectHeader">Cheflavor Staff Portal</h1>
                     <img onMouseEnter={handleHoverEnter} onMouseLeave={ handleHoverLeave } src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636855473/portfolio/staff-portal_owyzce.png" alt="cheflavor-website-gif" id="cheflavor-staffportal"/>
                     <div className="buttonDiv">
-                        <button className="projectButtons"><a href="https://protected-scrubland-89262.herokuapp.com/cheflavor/stafflogin/login">website</a></button>
-                        <button className="projectButtons"><a href="https://github.com/mtisby/cheflavor">repository</a></button>
+                        <button className="projectButtons"><a href="https://protected-scrubland-89262.herokuapp.com/cheflavor/stafflogin/login"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1637450368/paint-app/logos/Website_pt9szx.png" alt="website" /></a></button>
+                        <button className="projectButtons"><a href="https://github.com/mtisby/cheflavor"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1637450368/paint-app/logos/GitHub_qb49mi.png" alt="github-logo" /></a></button>
                     </div>
                 </div>
                 <div className="projectDiv">
                     <h1 className="projectHeader">Star Campsites</h1>
                 <img onMouseEnter={handleHoverEnter} onMouseLeave={ handleHoverLeave } src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636656190/portfolio/star-campsites_veouus.png" alt="star campsites website gif" id="star-campsites"/>
                     <div className="buttonDiv">
-                        <button className="projectButtons"><a href="https://secure-gorge-90469.herokuapp.com/">website</a></button>
-                        <button className="projectButtons"><a href="https://github.com/mtisby/star-campsites">repository</a></button>
+                        <button className="projectButtons"><a href="https://secure-gorge-90469.herokuapp.com/"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1637450368/paint-app/logos/Website_pt9szx.png" alt="website" /></a></button>
+                        <button className="projectButtons"><a href="https://github.com/mtisby/star-campsites"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1637450368/paint-app/logos/GitHub_qb49mi.png" alt="github-logo" /></a></button>
                     </div>
                 </div>
             </div>
@@ -86,16 +73,16 @@ const Portfolio = () => {
                     <h1 className="projectHeader">Events Calendar</h1>
                     <img onMouseEnter={handleHoverEnter} onMouseLeave={ handleHoverLeave } src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636658235/portfolio/js-calendar_brr61d.png" alt="cheflavor website gif" id="js-calendar"/>
                     <div className="buttonDiv">
-                        <button className="projectButtons"><a href="https://mtisby.github.io/javascript-calendar/">website</a></button>
-                        <button className="projectButtons"><a href="https://github.com/mtisby/javascript-calendar">repository</a></button>
+                        <button className="projectButtons"><a href="https://mtisby.github.io/javascript-calendar/"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1637450368/paint-app/logos/Website_pt9szx.png" alt="website" /></a></button>
+                        <button className="projectButtons"><a href="https://github.com/mtisby/javascript-calendar"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1637450368/paint-app/logos/GitHub_qb49mi.png" alt="github-logo" /></a></button>
                     </div>
                 </div>
                 <div className="projectDiv">
                     <h1 className="projectHeader">Signum</h1>
                 <img onMouseEnter={handleHoverEnter} onMouseLeave={ handleHoverLeave } src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636659030/portfolio/signum_yh3yez.png" alt="star campsites website gif" id="signum"/>
                     <div className="buttonDiv">
-                        <button className="projectButtons"><a href="https://ismatullaevash.github.io/signum/#/">website</a></button>
-                        <button className="projectButtons"><a href="https://github.com/ismatullaevash/signum">repository</a></button>
+                        <button className="projectButtons"><a href="https://ismatullaevash.github.io/signum/#/"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1637450368/paint-app/logos/Website_pt9szx.png" alt="website" /></a></button>
+                        <button className="projectButtons"><a href="https://github.com/ismatullaevash/signum"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1637450368/paint-app/logos/GitHub_qb49mi.png" alt="github-logo" /></a></button>
                     </div>
                 </div>
                 
