5cc8580b2abd46be15fda6a2d103a3cf244fefd0
mtisby <marieltisby1998@gmail.com>
2022-02-24 15:13:18 -0800

    updates

--- portfolio/src/pages/about_me/aboutme.jsx
+++ portfolio/src/pages/about_me/aboutme.jsx
@@ -7,7 +7,7 @@ function AboutMe() {
     <div className='aboutme'>
        <div class="mariel">
           <img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636672805/portfolio/picture_zb5vbf.jpg" alt="mariel" class="marielProfilePic" />
-          <p> Mariel is a cognitive scientist turned full stack developer. After five years of research, she realized her passion wasn't for research but for developing easily accessible tools.</p>
+          <p> Hi my name is Mar! I'm a full stack developer who loves to create experiences. I currently work at Mercury Intermedia but I am always working on my own personal projects.</p>
           <div class="contactus">
               <a href="https://github.com/mtisby"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645564388/portfolio/updated%20logos/dark%20mode/github_adzghe.png" alt="github"  /></a>
               <a target="_blank" href="mailto: marieltisby1998@gmail.com"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645564391/portfolio/updated%20logos/dark%20mode/email_sn5stt.png" alt="Email" className='logo' /></a>
--- portfolio/src/pages/projects/Projects.jsx
+++ portfolio/src/pages/projects/Projects.jsx
@@ -4,12 +4,11 @@ import "./projects.css"
 
 function Projects() {
   const projectGifs = {
-    "star-campsites": "https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636654003/portfolio/Star_Campsites_uliupa.gif",
-    "cheflavor": "https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636655757/portfolio/cheflavor_banyqt.gif",
-    "js-calendar": "https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636658371/portfolio/js-calenfdar_auidpm.gif",
-    "signum": "https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636659040/portfolio/signum_oslci3.gif",
-    "cheflavor-staffportal": "https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636855482/portfolio/cheflavor-staffportal_ijllwa.gif",
-    "poodle-doodle": "https://res.cloudinary.com/dr0ofxgkz/image/upload/v1637555977/paint-app/Poodle_Doodle_-_Google_Chrome_2021-11-21_20-21-49_SparkVideo_1_x0u3jj.gif"
+    "cheflavor": "https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645743922/portfolio/Screen_Shot_2022-02-24_at_3.04.33_PM_m0p2nr.png",
+    "cheflavor-staffportal": "https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645744022/portfolio/Screen_Shot_2022-02-24_at_3.06.38_PM_qt4qn2.png",
+    "poodle-doodle": "https://res.cloudinary.com/dr0ofxgkz/image/upload/v1637555977/paint-app/Poodle_Doodle_-_Google_Chrome_2021-11-21_20-21-49_SparkVideo_1_x0u3jj.gif",
+    "sheNetworks": "https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645736760/portfolio/Screen_Shot_2022-02-24_at_1.05.42_PM_suh7du.png",
+    "cinema-save": "https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645737116/portfolio/cinema-save_agg2ay.png"
   }
 
   const handleHoverEnter = (e) => {
@@ -34,7 +33,7 @@ function Projects() {
       <div className='projects-grid'>
         <div className="projectDiv grid-col-span-2 cinema-save">
             {/* <h1 className="projectHeader">Cinema Save</h1> */}
-            <img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645737116/portfolio/cinema-save_agg2ay.png" id="cinema save"/>
+            <img onMouseEnter={handleHoverEnter} onMouseLeave={ handleHoverLeave } src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645737116/portfolio/cinema-save_agg2ay.png" id="cinema-save"/>
             <div className="buttonDiv">
                 <a target="_blank" href="https://mtisby.github.io/cinema-save-client/"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645730403/portfolio/updated%20logos/dark%20mode/internet_gippym.png" alt="website logo" /></a>
                 <a target="_blank" href="https://github.com/mtisby/cinema-save-client"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645564388/portfolio/updated%20logos/dark%20mode/github_adzghe.png" alt="github repo logo" /></a>
@@ -42,7 +41,7 @@ function Projects() {
         </div>
         <div className="projectDiv cheflavor">
             {/* <h1 className="projectHeader">Cheflavor</h1> */}
-            <img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645742752/portfolio/Portfolio_rrh6em.png" alt="cheflavor website gif" id="cheflavor"/> 
+            <img onMouseEnter={handleHoverEnter} onMouseLeave={ handleHoverLeave } src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645742752/portfolio/Portfolio_rrh6em.png" alt="cheflavor website gif" id="cheflavor"/> 
             <div className="buttonDiv">
                 <a target="_blank" href="https://protected-scrubland-89262.herokuapp.com/cheflavor"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645730403/portfolio/updated%20logos/dark%20mode/internet_gippym.png" alt="website logo" /></a>
                 <a target="_blank" href="https://github.com/mtisby/cheflavor"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645564388/portfolio/updated%20logos/dark%20mode/github_adzghe.png" alt="github repo logo" /></a>
@@ -50,7 +49,7 @@ function Projects() {
         </div>
         <div className="projectDiv cheflavor-staff">
             {/* <h1 className="projectHeader">Cheflavor Staff Portal</h1> */}
-            <img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645742752/portfolio/cheflavor-staffportal_mcskfu.png" id="cheflavor-staffportal"/>
+            <img onMouseEnter={handleHoverEnter} onMouseLeave={ handleHoverLeave } src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645742752/portfolio/cheflavor-staffportal_mcskfu.png" id="cheflavor-staffportal"/>
             <div className="buttonDiv">
                 <a target="_blank" href="https://protected-scrubland-89262.herokuapp.com/cheflavor/stafflogin/login"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645730403/portfolio/updated%20logos/dark%20mode/internet_gippym.png" alt="website logo" /></a>
                 <a target="_blank" href="https://github.com/mtisby/cheflavor"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645564388/portfolio/updated%20logos/dark%20mode/github_adzghe.png" alt="github repo logo" /></a>
@@ -59,7 +58,7 @@ function Projects() {
 
         <div className="projectDiv grid-col-span-2 poodle-doodle">
             {/* <h1 className="projectHeader">Poodle Doodle</h1> */}
-            <img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645742752/portfolio/poodle-doodle_m9fv1l.png" alt="poodle doodle website gif" id="poodle-doodle"/>
+            <img onMouseEnter={handleHoverEnter} onMouseLeave={ handleHoverLeave } src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645742752/portfolio/poodle-doodle_m9fv1l.png" alt="poodle doodle website gif" id="poodle-doodle"/>
             <div className="buttonDiv">
                 <a target="_blank" href="https://pacific-bastion-50258.herokuapp.com/"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645730403/portfolio/updated%20logos/dark%20mode/internet_gippym.png" alt="website logo" /></a>
                 <a target="_blank" href="https://github.com/mtisby/paint-app"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645564388/portfolio/updated%20logos/dark%20mode/github_adzghe.png" alt="github repo logo" /></a>
@@ -68,7 +67,7 @@ function Projects() {
 
         <div className="projectDiv grid-col-span-2 sheNetworks">
             {/* <h1 className="projectHeader">sheNetworks</h1> */}
-            <img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645742963/portfolio/sheNetworks_qnvk1z.png" id="sheNetworks"/> 
+            <img onMouseEnter={handleHoverEnter} onMouseLeave={ handleHoverLeave } src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645742963/portfolio/sheNetworks_qnvk1z.png" id="sheNetworks"/> 
             <div className="buttonDiv">
                 <a target="_blank" href="https://quiet-anchorage-71064.herokuapp.com/"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645730403/portfolio/updated%20logos/dark%20mode/internet_gippym.png" alt="website logo" /></a>
                 <a target="_blank" href="https://github.com/mtisby/she-hacks"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645564388/portfolio/updated%20logos/dark%20mode/github_adzghe.png" alt="github repo logo" /></a>
