3fa7cc55377304512ca7469f3aa7303f936e07df
mtisby <marieltisby1998@gmail.com>
2022-08-21 11:57:01 -0700

    fix link

--- portfolio/src/pages/about_me/aboutme.jsx
+++ portfolio/src/pages/about_me/aboutme.jsx
@@ -7,7 +7,7 @@ function AboutMe() {
     <div className='aboutme'>
        <div class="mariel">
           <img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636672805/portfolio/picture_zb5vbf.jpg" alt="mariel" class="marielProfilePic" />
-          <p> Hi my name is Mar! I'm a full stack developer who loves to create experiences. I currently work at <a href="http://mercury.io/" className='mercury'>Mercury Intermedia</a> building APIs and UI for clients.</p>
+          <p> Hi my name is Mar! I'm a full stack developer who loves to create experiences. I currently work at <a href="http://mercury.io/" target="_blank" className='mercury'>Mercury Intermedia</a> building APIs and UI for clients.</p>
           <div class="contactus">
               <a target="_blank" href="https://github.com/mtisby"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645564388/portfolio/updated%20logos/dark%20mode/github_adzghe.png" alt="github"  /></a>
               <a target="_blank" href="mailto: marieltisby1998@gmail.com"><img src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1645564391/portfolio/updated%20logos/dark%20mode/email_sn5stt.png" alt="Email" className='logo' /></a>
